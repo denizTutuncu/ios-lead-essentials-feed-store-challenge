8e8ea2f98fd08ee1afa38230d7c0d01efb445dd5
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-22 23:16:07 -0700

    `CoreDataFeedStore` retrieve delivers found values on non-empty cache

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -29,26 +29,68 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func retrieve(completion: @escaping RetrievalCompletion) {
-		completion(.empty)
+		perform { context in
+			do {
+				let request = NSFetchRequest<ManagedCache>(entityName: ManagedCache.entity().name!)
+				request.returnsObjectsAsFaults = false
+				if let cache = try context.fetch(request).first {
+					let feed = cache.feed.compactMap { ($0 as? ManagedFeedImage) }.map {
+						LocalFeedImage(id: $0.id, description: $0.imageDescription, location: $0.location, url: $0.url)
+					}
+
+					completion(.found(feed: feed, timestamp: cache.timestamp))
+				} else {
+					completion(.empty)
+				}
+			} catch {
+				completion(.failure(error))
+			}
+		}
 	}
 
 	public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-		fatalError("Must be implemented")
+		perform { context in
+			do {
+				let managedCache = ManagedCache(context: context)
+				managedCache.timestamp = timestamp
+				managedCache.feed = NSOrderedSet(array: feed.map { local in
+					let managedFeed = ManagedFeedImage(context: context)
+					managedFeed.id = local.id
+					managedFeed.imageDescription = local.description
+					managedFeed.location = local.location
+					managedFeed.url = local.url
+
+					return managedFeed
+				})
+
+				try context.save()
+				completion(nil)
+			} catch {
+				completion(error)
+			}
+		}
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
 	}
 
+	private func perform(_ action: @escaping (NSManagedObjectContext) -> Void) {
+		let context = self.context
+		context.perform { action(context) }
+	}
+
+	@objc(ManagedCache)
 	private class ManagedCache: NSManagedObject {
 		@NSManaged var timestamp: Date
 		@NSManaged var feed: NSOrderedSet
 	}
 
+	@objc(ManagedFeedImage)
 	private class ManagedFeedImage: NSManagedObject {
 		@NSManaged var id: UUID
 		@NSManaged var imageDescription: String?
-		@NSManaged var imageLocation: String?
+		@NSManaged var location: String?
 		@NSManaged var url: URL
 		@NSManaged var cache: ManagedCache
 	}

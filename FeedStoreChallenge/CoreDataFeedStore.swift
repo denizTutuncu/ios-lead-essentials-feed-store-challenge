d8cb741b5bbd0a88817a63589ec1679bd1576740
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-08-22 23:12:04 -0700

    Add `ManagedCache` and `ManagedFeedImage` model representations to `CoreDataFeedStore`

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -39,4 +39,17 @@ public final class CoreDataFeedStore: FeedStore {
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
 		fatalError("Must be implemented")
 	}
+
+	private class ManagedCache: NSManagedObject {
+		@NSManaged var timestamp: Date
+		@NSManaged var feed: NSOrderedSet
+	}
+
+	private class ManagedFeedImage: NSManagedObject {
+		@NSManaged var id: UUID
+		@NSManaged var imageDescription: String?
+		@NSManaged var imageLocation: String?
+		@NSManaged var url: URL
+		@NSManaged var cache: ManagedCache
+	}
 }
